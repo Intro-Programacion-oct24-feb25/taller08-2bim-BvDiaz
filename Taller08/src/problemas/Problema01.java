ce57ba5a32f85c7a7dc877028cb26e8ab8ce288f
Bryan Diaz <vicentepomas.2007@gmail.com>
2024-12-05 12:16:33 -0500

    PROBLEMAS TALLER

--- Taller08/src/problemas/Problema01.java
+++ Taller08/src/problemas/Problema01.java
@@ -14,7 +14,17 @@ public class Problema01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int operacion;
+        String cadena = "";
+        for (int i = 30; i >= 10; i--) {
+            cadena = String.format("%s%d", cadena, i);
+            for (int d = 2; d <= 4; d++) {
+                operacion = i * d;
+                cadena = String.format("%s-%d", cadena,
+                        operacion);
+            }
+            cadena = String.format("%s\n", cadena);
+        }
+        System.out.printf("%s\n", cadena);
     }
-    
 }
--- Taller08/src/problemas/Problema02.java
+++ Taller08/src/problemas/Problema02.java
@@ -14,7 +14,20 @@ public class Problema02 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        double fahrenheit = 20;
+        double incremento = 4;
+        double acumulador = 0;
+
+        for (int i = 0; i <= 20; i++) {
+            double celsius = (fahrenheit - 32) * 5 / 9;
+            acumulador = acumulador + celsius;
+
+            System.out.printf("Operación(%d) Fahrenheit: %.2f ===> "
+                    + "Celsius: %.2f\n", i, fahrenheit, celsius);
+
+            fahrenheit = fahrenheit + incremento;
+        }
+
+        System.out.println("\nAcumulador de Celsius: " + acumulador);
     }
-    
 }
